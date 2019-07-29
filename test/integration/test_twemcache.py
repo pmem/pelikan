b9c5b7cf372869aca647409f364d14874ac38c55
Tomek <Tomek.id97@gmail.com>
2019-07-29 13:15:06 +0200

    add pmem test scenario for ungraceful shutdown

--- test/integration/test_twemcache.py
+++ test/integration/test_twemcache.py
@@ -1,18 +1,24 @@
 from base import GenericTest
+from base import GenericPmemTest
 
-from os import listdir
+import os
 import unittest
 
+def defineTest(suite, fname, test_type):
+    test = type()
+    test.load('twemcache/' + fname)
+    suite.addTest(test)
 
 def twemcache():
     suite = unittest.TestSuite()
-    for fname in listdir('twemcache'):
-        test = GenericTest()
-        test.load('twemcache/' + fname)
-        suite.addTest(test)
+    for fname in os.listdir('twemcache'):
+        defineTest(suite, fname, GenericTest)
+        defineTest(suite, fname, GenericPmemTest)
 
     return suite
 
 
 if __name__ == '__main__':
     unittest.TextTestRunner(verbosity=2).run(twemcache())
+    if os.path.exists("/dev/shm/pmem"):
+        os.remove("/dev/shm/pmem")
